f0f75f4b2c41c095242727f165a59f64b26ed9a1
Abdul <abdul@jobtech.sg>
2025-07-12 15:09:27 +0800

    add app.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { GitHubProvider } from './context/GithubContext.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GitHubProvider } from "./context/GithubContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <GitHubProvider>
-    <App />
-  </GitHubProvider>,
-)
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GitHubProvider>
+);
